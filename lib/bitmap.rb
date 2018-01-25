24feddc05168e7c16e8a567e73ddf10f8be95e33
Dmitry Zeldin <dmitry@zeldin.pro>
2018-01-25 23:18:54 +0100

    Oups. Too much Go for today

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,36 +1,36 @@
 # Bitmap is a representation of a bitmap as an M x N matrix of pixels,
 # where each element represents a colour
-class Bitmap {
+class Bitmap
   # Initialize an empty bitmap of size n, m and fill it with white colour
   # represented by character O
-  def initialize(n, m) {
+  def initialize(n, m)
 
-  }
+  end
 
   # Clear the bitmap by setting all pixels to white (O)
-  def clear() {
+  def clear()
 
-  }
+  end
 
   # Colour the pixel (x, y) with colour c
-  def set_colour(x, y, c) {
+  def set_colour(x, y, c)
 
-  }
+  end
 
   # Draw a vertical segment of colour c in column x between
   # rows y1 and y2 (inclusive)
-  def vertical_segment(x, y1, y2, c) {
+  def vertical_segment(x, y1, y2, c)
 
-  }
+  end
 
   # Draw a horizontal segment of colour c in row y between
   # columns x1 and x2 (inclusive)
-  def horizontal_segment(x1, x2, y, c) {
+  def horizontal_segment(x1, x2, y, c)
 
-  }
+  end
 
   # Print the current state of a bitmap.
-  def to_s() {
+  def to_s()
 
-  }
-}
+  end
+end

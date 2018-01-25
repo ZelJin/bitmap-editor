f897f7588b3b03c87770fefceff1801e4b786986
Dmitry Zeldin <dmitry@zeldin.pro>
2018-01-25 23:19:29 +0100

    Initialized an empty spec.

---
+++ spec/bitmap_spec.rb
@@ -0,0 +1,27 @@
+require 'bitmap'
+
+RSpec.describe Bitmap do
+  describe '#initialize' do
+
+  end
+
+  describe '#clear' do
+
+  end
+
+  describe '#set_colour' do
+
+  end
+
+  describe '#vertical_segment' do
+
+  end
+
+  describe '#horizontal_segment' do
+
+  end
+
+  describe '#to_s' do
+
+  end
+end

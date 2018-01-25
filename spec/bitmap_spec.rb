f251a81f2ee7adfaa8bb53005b3d18bc102604fd
Dmitry Zeldin <dmitry@zeldin.pro>
2018-01-26 00:45:16 +0100

    Implemented test cases for set_colour

--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -53,7 +53,44 @@ RSpec.describe Bitmap, '#clear' do
 end
 
 RSpec.describe Bitmap, '#set_colour' do
+  before (:each) do
+    @bitmap = Bitmap.new(25, 25)
+  end
 
+  context 'when n or m are out of bounds' do
+    it 'should raise an error' do
+      expect { @bitmap.set_colour(26, 1, 'C') }.to raise_error ArgumentError
+      expect { @bitmap.set_colour(4, -1, 'C') }.to raise_error ArgumentError
+      expect { @bitmap.set_colour(-5, 100500, 'C') }.to raise_error ArgumentError
+      expect { @bitmap.set_colour(0, 49, 'C') }.to raise_error ArgumentError
+    end
+  end
+
+  context 'when colour string is incorrect' do
+    it 'should raise an error' do
+      expect { @bitmap.set_colour(1, 1, 'Ж') }.to raise_error ArgumentError
+      expect { @bitmap.set_colour(1, 1, 'CS') }.to raise_error ArgumentError
+      expect { @bitmap.set_colour(1, 1, 42) }.to raise_error ArgumentError
+      expect { @bitmap.set_colour(1, 1, nil) }.to raise_error ArgumentError
+      expect { @bitmap.set_colour(1, 1, 'k') }.to raise_error ArgumentError
+      expect { @bitmap.set_colour(1, 1, '9') }.to raise_error ArgumentError
+      expect { @bitmap.set_colour(1, 1, '@') }.to raise_error ArgumentError
+      expect { @bitmap.set_colour(1, 1, ';') }.to raise_error ArgumentError
+      expect { @bitmap.set_colour(1, 1, ' ') }.to raise_error ArgumentError
+      expect { @bitmap.set_colour(1, 1, '🤷') }.to raise_error ArgumentError
+    end
+  end
+
+  context 'when parameters are correct' do
+    it 'should paint the correct pixel' do
+      @bitmap.set_colour(1, 1, 'C')
+      expect(@bitmap.bitmap[1-1][1-1]).to eq('C')
+      @bitmap.set_colour(2, 2, 'D')
+      expect(@bitmap.bitmap[2-1][2-1]).to eq('D')
+      @bitmap.set_colour(24, 25, 'E')
+      expect(@bitmap.bitmap[24-1][25-1]).to eq('E')
+    end
+  end
 end
 
 RSpec.describe Bitmap, '#vertical_segment' do

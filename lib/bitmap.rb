563f9fefa6188b827fc0e53f45d506cec08fde0a
Dmitry Zeldin <dmitry@zeldin.pro>
2018-01-25 23:06:56 +0100

    initialized an empty Bitmap class

---
+++ lib/bitmap.rb
@@ -0,0 +1,36 @@
+# Bitmap is a representation of a bitmap as an M x N matrix of pixels,
+# where each element represents a colour
+class Bitmap {
+  # Initialize an empty bitmap of size n, m and fill it with white colour
+  # represented by character O
+  def initialize(n, m) {
+
+  }
+
+  # Clear the bitmap by setting all pixels to white (O)
+  def clear() {
+
+  }
+
+  # Colour the pixel (x, y) with colour c
+  def set_colour(x, y, c) {
+
+  }
+
+  # Draw a vertical segment of colour c in column x between
+  # rows y1 and y2 (inclusive)
+  def vertical_line(x, y1, y2, c) {
+
+  }
+
+  # Draw a horizontal segment of colour c in row y between
+  # columns x1 and x2 (inclusive)
+  def horizontal_line(x1, x2, y, c) {
+
+  }
+
+  # Print the current state of a bitmap.
+  def to_s() {
+
+  }
+}

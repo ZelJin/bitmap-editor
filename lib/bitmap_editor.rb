46052373c1724e454a51e9a34169ec8eceaa821d
Dmitry Zeldin <dmitry.zeldin@allianz.de>
2018-01-26 11:58:32 +0100

    Included string to int conversion in the command

--- lib/bitmap_editor.rb
+++ lib/bitmap_editor.rb
@@ -14,7 +14,8 @@ class BitmapEditor
         next unless args_correct?(args, 3, 'I N M')
         # Try to create a bitmap. Catch an ArgumentError if it occurs
         begin
-          bitmap = Bitmap.new(args[1], args[2])
+          bitmap = Bitmap.new(args[1].to_i, args[2].to_i)
+          puts bitmap
         rescue ArgumentError => err
           puts 'Argument error: ' + err.message
         end
@@ -27,7 +28,7 @@ class BitmapEditor
         next unless bitmap_present?(bitmap) && args_correct?(args, 4, 'L X Y C')
         # Try to colour a pixel. Catch an ArgumentError if it occurs
         begin
-          bitmap.set_colour(args[1], args[2], args[3])
+          bitmap.set_colour(args[1].to_i, args[2].to_i, args[3])
         rescue ArgumentError => err
           puts 'Argument error: ' + err.message
         end
@@ -36,7 +37,7 @@ class BitmapEditor
         next unless bitmap_present?(bitmap) && args_correct?(args, 5, 'V X Y1 Y2 C')
         # Try to draw a vertical segment. Catch an ArgumentError if it occurs
         begin
-          bitmap.vertical_segment(args[1], args[2], args[3], args[4])
+          bitmap.vertical_segment(args[1].to_i, args[2].to_i, args[3].to_i, args[4])
         rescue ArgumentError => err
           puts 'Argument error: ' + err.message
         end
@@ -45,7 +46,7 @@ class BitmapEditor
         next unless bitmap_present?(bitmap) && args_correct?(args, 5, 'H X1 X2 Y C')
         # Try to draw a horizontal segment. Catch an ArgumentError if it occurs
         begin
-          bitmap.horizontal_segment(args[1], args[2], args[3], args[4])
+          bitmap.horizontal_segment(args[1].to_i, args[2].to_i, args[3].to_i, args[4])
         rescue ArgumentError => err
           puts 'Argument error: ' + err.message
         end
